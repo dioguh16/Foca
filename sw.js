2ebe463aa9b893bff571ddc1a790da9644328860
dioguh16 <114818101+dioguh16@users.noreply.github.com>
2026-07-27 18:54:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'tracker-v3';
+const CACHE_NAME = 'tracker-v4';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './app.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
